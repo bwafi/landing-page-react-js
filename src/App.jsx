d3e6dbce083a444923535746b91312a9a2685850
Syahroni Bawafi <syahronibwf@gmail.com>
2023-04-04 00:41:48 +0700

    bug img

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App() {
         </div>
 
         <div className="absolute right-72 top-20">
-          <img src="/public/img/image-1.jpg" alt="" className="h-96 w-96 rounded-t-full" />
+          <img src="/img/image-1.jpg" alt="image-1" className="h-96 w-96 rounded-t-full" />
         </div>
 
         <div className="absolute left-[10%] top-[40%] z-10">
@@ -59,15 +59,15 @@ function App() {
         </div>
 
         <div className="absolute left-[5%] top-[45%]">
-          <img src="/public/svg/dot.svg" alt="" />
+          <img src="/svg/dot.svg" alt="dot" />
         </div>
 
         <div className="absolute left-52 top-[45%]">
-          <img src="/public/img/image-2.jpg" alt="" className="rounded-b-full" />
+          <img src="/img/image-2.jpg" alt="image-2" className="rounded-b-full" />
         </div>
 
         <div className="absolute right-1 top-[47%]">
-          <img src="/public/img/image-3.jpg" alt="" className="w-[280px] rounded-b-full" />
+          <img src="/img/image-3.jpg" alt="image-3" className="w-[280px] rounded-b-full" />
         </div>
       </section>
 
@@ -85,7 +85,7 @@ function App() {
               dolore magna aliqua. Ut enim ad minim veniam.”
             </p>
             <div className="mt-5 flex items-center">
-              <img src="/public/img/Bitmap.jpg" alt="" className="mr-4 rounded-full" />
+              <img src="/img/Bitmap.jpg" alt="bitmap" className="mr-4 rounded-full" />
               <div className="flex flex-col justify-center">
                 <h3 className="text-sm font-semibold">Samantha William</h3>
                 <p className="text-xs">Owner of Online Fashion Store</p>
@@ -96,14 +96,14 @@ function App() {
 
         <div className="relative flex w-1/2 flex-col items-center justify-end py-7">
           <div className="mr-12 flex w-full justify-end">
-            <img src="/public/img/image-4.jpg" alt="" className="h-[400px] w-80 rounded-t-full" />
+            <img src="/img/image-4.jpg" alt="image-4" className="h-[400px] w-80 rounded-t-full" />
             <p className=" absolute right-1/2 top-48 w-52 rounded-t-2xl rounded-bl-2xl bg-[#7D6353] px-5 py-3 text-sm font-medium text-white">
               Hi Karen, I’m wandering is it possible to order custom drawing?
             </p>
           </div>
 
           <div className="relative flex w-full items-center">
-            <img src="/public/img/image-5.jpg" alt="" className="h-[400px] w-80 rounded-b-full" />
+            <img src="/img/image-5.jpg" alt="image-5" className="h-[400px] w-80 rounded-b-full" />
             <div className="absolute right-24 top-16 flex flex-col">
               <p className="mb-3 w-36 rounded-full bg-[#726985] px-5 py-3 text-sm font-medium text-white">
                 Hi Samantha
@@ -119,9 +119,9 @@ function App() {
       <section className="my-10 flex w-full justify-around">
         <div className="relative flex w-1/2 flex-col items-center ">
           <div className="absolute left-2/3 top-16 flex h-44 w-44 items-center justify-center rounded-full border border-[#3C636A] ">
-            <img src="/public/img/image-7.jpg" alt="" className=" h-28 w-28 rounded-full" />
+            <img src="/img/image-7.jpg" alt="image-7" className=" h-28 w-28 rounded-full" />
           </div>
-          <img src="/public/img/image-6.jpg" alt="" className="h-[650px] w-[400px] rounded-full" />
+          <img src="/img/image-6.jpg" alt="image-6" className="h-[650px] w-[400px] rounded-full" />
           <div className="w-full">
             <div className="video-circle relative bottom-28 left-16 flex h-14 w-[200px] items-center rounded-full bg-white">
               <div className="absolute left-16">
@@ -143,7 +143,7 @@ function App() {
                 dolore magna aliqua. Ut enim ad minim veniam.”
               </p>
               <div className="mt-5 flex items-center">
-                <img src="/public/img/Bitmap-2.jpg" alt="" className="mr-4 h-12 w-12 rounded-full" />
+                <img src="/img/Bitmap-2.jpg" alt="bitmap-2" className="mr-4 h-12 w-12 rounded-full" />
                 <div className="flex flex-col justify-center">
                   <h3 className="text-sm font-semibold">Samantha William</h3>
                   <p className="text-xs">Owner of Online Fashion Store</p>
